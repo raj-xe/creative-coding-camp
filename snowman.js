e073c1ecc23fd87ce0ef57d065cbadd3e3977827
raj-xe <rdhanani@codal.com>
2021-07-12 11:29:13 -0500

    final snowman part -1

--- snowman.js
+++ snowman.js
@@ -3,8 +3,12 @@ function setup() {
 }
 
 function draw() {
+
     background(255);
     rect(0, 300, 400, 100);
     ellipse(200, 300, 150, 150);
     ellipse(200, 200, 100, 100);
+    ellipse(200, 125, 75, 75);
+    line(160, 200, 160 - 75, 200 - 75);
+    line(240, 200, 240 + 75, 200 - 75);
 }
