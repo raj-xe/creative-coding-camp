95c58c3cdc693f044947caeeb26ca94b254a887e
raj-xe <rdhanani@codal.com>
2021-07-12 11:13:26 -0500

    init snowman task

---
+++ snowman.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400)
+}
+
+function draw() {
+    background(255);
+    rect(0, 300, 400, 100);
+    ellipse(200, 300, 150, 150);
+    ellipse(200, 200, 100, 100);
+}
